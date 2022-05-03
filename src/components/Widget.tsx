13c44ab5ee21ee2985a5f9d34c06ab0d0f77f8d6
Ana Perola <anaperolinha@hotmail.com>
2022-05-02 21:53:18 -0300

    Includ useSate in component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,25 @@
 import { ChatTeardropDots } from "phosphor-react"
+import { useState } from "react"
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibity(){
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
   return (
-    <button className="bg-violet-500">
+   <div className="absolute bottom-4 right-5">
+
+    {isWidgetOpen && <p>Hello </p>}
+    <button onClick={toggleWidgetVisibity} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
       <ChatTeardropDots className="w-6 h-6"/>
+
+      <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+        <span className="pl-2"></span>
+        Feedback
+      </span>
     </button>
+   </div>
   )
 }
