927c5f9eb53d7948d5599d3b4bd691f18484d404
Ana Perola <anaperolinha@hotmail.com>
2022-05-04 20:30:16 -0300

    Add import CloseButton to WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,10 +1,15 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu comentário</span>
+        <CloseButton />
       </header>
+
       <p>Hello word</p>
+      
       <footer className="text-xs text-neutral-400">
       Feito com ♥ pela <a className="underline underline-offset-2" href="https://github.com/AnaPerola">Ana</a>
       </footer>
