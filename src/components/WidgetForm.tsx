f6c8854c95cbbfc4fd7f3985ba90fc3733ba8366
Ana Perola <anaperolinha@hotmail.com>
2022-05-04 20:09:27 -0300

    Add responsiveness in component

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,6 +1,6 @@
 export function WidgetForm() {
   return(
-    <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg">
+    <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       hello word
     </div>
   )
