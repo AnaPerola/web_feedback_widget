f26482716447c2508ef7d834b5b4907fdbcb775f
Ana Perola <anaperolinha@hotmail.com>
2022-05-04 20:23:50 -0300

    Create header and footer to WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,13 @@
 export function WidgetForm() {
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      hello word
+      <header>
+        <span className="text-xl leading-6">Deixe seu comentário</span>
+      </header>
+      <p>Hello word</p>
+      <footer className="text-xs text-neutral-400">
+      Feito com ♥ pela <a className="underline underline-offset-2" href="https://github.com/AnaPerola">Ana</a>
+      </footer>
     </div>
   )
 }
