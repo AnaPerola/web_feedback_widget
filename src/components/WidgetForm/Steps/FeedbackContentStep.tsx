810351704e2720118b62cec1c6fdd351db112263
Ana Perola <anaperolinha@hotmail.com>
2022-05-04 22:40:02 -0300

    Add return with header and content of component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,3 +1,15 @@
+import { CloseButton } from "../../CloseButton"
+
 export function FeedbackContentStep() {
-  return()
+  return(
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu comentário</span>
+        
+        <CloseButton />
+      </header>
+      <div className="flex py-8 gap-2 w-full">
+      </div>
+    </>
+  )
 }
