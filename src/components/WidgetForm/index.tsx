1e9983c38c5bec5e7b70881a92862db28a3d7623
Ana Perola <anaperolinha@hotmail.com>
2022-05-07 18:52:56 -0300

    Add const chang sent

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -38,12 +38,13 @@ export function WidgetForm() {
 
   function handleRestartFeedback() {
     setFeedbackType(null);
+    setFeedbackSent(false);
   }
 
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       { feedbackSent ? (
-        <FeedbackSuccessStep />
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
       ) : (
         <>
           {!feedbackType ? (
