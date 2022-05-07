7f1570e8f0432de89212477257e3260a32421d0f
Ana Perola <anaperolinha@hotmail.com>
2022-05-07 18:30:35 -0300

    Include new condition to render components

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -4,6 +4,7 @@ import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep ';
 
 export const feedbackTypes = {
   BUG: {
@@ -42,16 +43,21 @@ export function WidgetForm() {
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
     
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+      { feedbackSent ? (
+        <FeedbackSuccessStep />
       ) : (
-        <FeedbackContentStep 
-          feedbackType={feedbackType}
-          onFeedbackRestartRequested={handleRestartFeedback}
-          onFeedbackSent={() => setFeedbackSent(true)}
-        />
-      )}
-      
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+          ) : (
+            <FeedbackContentStep 
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      ) }
       
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://github.com/AnaPerola">Ana</a>
