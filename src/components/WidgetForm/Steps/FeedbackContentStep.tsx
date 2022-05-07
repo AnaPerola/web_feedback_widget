2bf3e3d32d29705113d98d9242072b97caabd2eb
Ana Perola <anaperolinha@hotmail.com>
2022-05-07 16:18:16 -0300

    Add icon and functionality for back action

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,14 +1,37 @@
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton"
 
-export function FeedbackContentStep() {
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+export function FeedbackContentStep({
+    feedbackType, 
+    onFeedbackRestartRequested
+  }: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
   return(
     <>
       <header>
-        <span className="text-xl leading-6">Deixe seu comentário</span>
-        
+        <button 
+          type="button" 
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4"/>
+        </button>
+
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
+          {feedbackTypeInfo.title} 
+        </span>
+
         <CloseButton />
       </header>
       <div className="flex py-8 gap-2 w-full">
+        
       </div>
     </>
   )
