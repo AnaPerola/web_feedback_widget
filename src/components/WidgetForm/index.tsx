f8ab1f8249fc43917971b403cfc59eedb400a4e9
Ana Perola <anaperolinha@hotmail.com>
2022-05-12 20:46:03 -0300

    Fix name copyright

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -60,7 +60,7 @@ export function WidgetForm() {
       ) }
       
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela <a className="underline underline-offset-2" href="https://github.com/AnaPerola">Ana</a>
+        Feito com ♥ pela <a className="underline underline-offset-2" href="https://github.com/AnaPerola">Ana Pérola</a>
       </footer>
     </div>
   )
